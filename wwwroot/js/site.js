b09975cf074450632699424a4845cce3636ced02
asifmahmud <amahmud@uci.edu>
2017-08-25 20:37:29 -0700

    added bower

---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// site.js
+
+(function () {
+    $('#username').text("Asif Mahmud");
+
+    var menuItems = $(".menu li a");
+    menuItems.on("click", function () {
+        alert("Hello");
+    });
+})();
