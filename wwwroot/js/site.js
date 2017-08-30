b8e6a01d9f7507e372ca4ca6af355c6421f5da6d
asifmahmud <amahmud@uci.edu>
2017-08-29 22:08:58 -0700

    added bootstrap

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,17 @@
-﻿// site.js
-
+﻿
 (function () {
     var $sidebarAndContainer = $("#sidebar, #container");
+    var $iconToggle = $("#toggleSidebar i.fa");
+
     $("#toggleSidebar").on("click", function () {
         $sidebarAndContainer.toggleClass("hide-sidebar");
         if ($sidebarAndContainer.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
+            $iconToggle.removeClass("fa-angle-left");
+            $iconToggle.addClass("fa-angle-right");
         }
         else {
-            $(this).text("Hide Sidebar");
+            $iconToggle.removeClass("fa-angle-right");
+            $iconToggle.addClass("fa-angle-left");
         }
     });
 })();
