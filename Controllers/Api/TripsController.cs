8671fda7dc759cf304d243290f5cc122dce79873
asifmahmud <amahmud@uci.edu>
2017-09-04 23:33:44 -0700

    added TripsController

---
+++ Controllers/Api/TripsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using trippy.Models;
+
+namespace trippy.Controllers.Api
+{
+    public class TripsController : Controller
+    {
+        private IWorldRepository _repository;
+
+        public TripsController(IWorldRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+
+            try
+            {
+                return Ok(_repository.GetAllTrips());
+            }
+            catch(Exception ex)
+            {
+                string Message = string.Format("Something went wrong: {0}", ex.Message);
+                return BadRequest(Message);
+            }
+        }
+    }
+}
