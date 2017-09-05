d5458a493cd24f297d412b51ab338c67ed3b2a87
asifmahmud <amahmud@uci.edu>
2017-09-05 00:31:47 -0700

    updated TripsController api

--- Controllers/Api/TripsController.cs
+++ Controllers/Api/TripsController.cs
@@ -11,10 +11,12 @@ namespace trippy.Controllers.Api
     public class TripsController : Controller
     {
         private IWorldRepository _repository;
+        private WorldContext _context;
 
-        public TripsController(IWorldRepository repository)
+        public TripsController(IWorldRepository repository, WorldContext context)
         {
             _repository = repository;
+            _context = context;
         }
 
         [HttpGet("api/trips")]
@@ -24,6 +26,21 @@ namespace trippy.Controllers.Api
             try
             {
                 return Ok(_repository.GetAllTrips());
+            }
+            catch(Exception ex)
+            {
+                string Message = string.Format("Something went wrong: {0}", ex.Message);
+                return BadRequest(Message);
+            }
+        }
+
+        [HttpPost("api/trips")]
+        public IActionResult Post([FromBody]Trip trip)
+        {
+            try
+            {
+                return Ok(true);
+                //return Ok(_context.Trips.Add(trip));
             }
             catch(Exception ex)
             {
